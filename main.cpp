4a10660036922a7410bf610a6fd26c637a0d17a3
MateusM312 <mateusmendes2626@gmail.com>
2026-06-11 16:07:31 -0300

    praticamente terminado

--- main.cpp
+++ main.cpp
@@ -50,6 +50,8 @@ class Pessoas{
 class NPC : public Pessoas{
     private:
         double coracao = 0;
+        bool namorando = false;
+        bool casado = false;
     public:
         NPC(string nome, int idade, int presente){
             setInfo(nome, idade, presente);
@@ -61,6 +63,22 @@ class NPC : public Pessoas{
 
         string getfavPresent(){
             return presente;
+        }
+        double getCoracao(){
+            return coracao;
+        }
+
+        bool getNamorando(){
+            return namorando;
+        }
+
+        bool getCasado(){
+            return casado;
+        }
+
+        bool setRelacionamento(bool namoro, bool casando){
+            namorando = namoro;
+            casado = casando;
         }
 
         void flertar(string tipo, double qtd){
@@ -77,8 +95,8 @@ class NPC : public Pessoas{
 };
 
 class Jogador : public Pessoas{
-    private:
-        double dinheiro;
+    protected:
+        double dinheiro = 0;
     public:
         Jogador(string n, int i, int pre){
             setInfo(n, i, 0); //voltar aqui
@@ -89,8 +107,48 @@ class Jogador : public Pessoas{
             cout << "Você comprou: " << item << endl;
         }
 
+        void pedirNamoro(NPC* npc){
+            if (npc->getNamorando() == true){
+                cout << "\nVocê já está namorando!!\n";
+                return;
+            }
+            if (npc->getCoracao() < 50){
+                cout << "\nVocês ainda não são chegados o suficiente!\n";
+                return;
+            }
+            cout << "\nParabens!! Agora vocês estão namorando!!\n";
+            npc->setRelacionamento(true, false);
+        }
+
+        void pedirCasamento(NPC* npc){
+            if (npc->getCasado() == true){
+                cout << "\nVocê já está casado!!\n";
+                return;
+            }
+            if (npc->getCoracao() < 100){
+                cout << "\nVocês ainda não são chegados o suficiente!\n";
+                return;
+            }
+            if (npc->getNamorando() == false){
+                cout << "\nVocês ainda não estão namorando!\n";
+                return; // faltava esse return!
+            }
+            cout << "\nParabens!! Agora vocês estão casados!!\n";
+            npc->setRelacionamento(true, true);
+
+            // Tela de fim de jogo
+            sleep_seconds(1);
+            system("cls");
+            cout << "\033[33m";
+            cout << "\nOBRIGADO POR JOGAR\n";
+            cout << "\033[0m";
+            cout << "\nParabéns!! Você zerou o jogo!\n";
+            cout << "O jogo fechará em 4 segundos...\n";
+            sleep_seconds(4);
+            exit(0); // fecha o programa
+        }
+
         void Presentear(NPC* npc, int itemIdx){
-            cout << "Selecione a pessoa que você deseja presentear: ";
             if (itemIdx < 0 || itemIdx >= inventario.size()) {
                 cout << "Item invalido!\n"; return;
             }
@@ -98,6 +156,9 @@ class Jogador : public Pessoas{
             cout << "Você presenteou " << npc->getName() << " com " << inventario[itemIdx] << "!\n";
             if (inventario[itemIdx] == npc->getfavPresent()){
                 npc->flertar("presente", 1);
+                cout << "\033[32m";
+                cout << "Bonus de +1 pois esse é o predente favorito de " << npc->getName() << endl;
+                cout << "\033[0m";
                 }else{
                     npc->flertar("presente", 0.5);
                 }
@@ -119,6 +180,7 @@ class Jogador : public Pessoas{
             return dinheiro;
         }
 }; 
+
 
 int main(){
     int escolha, idade, presenteFav;
@@ -166,6 +228,7 @@ int main(){
         cout << "5. Pedir em casamento" << endl;
         cout << "6. Verificar o inventário" << endl;
         cout << "7. Exibir pretendentes" << endl;
+        cout << "9. Debug" << endl;
         cout << "0. Sair" << endl;
 
         cout << "O que voce deseja fazer? "; 
@@ -254,7 +317,12 @@ int main(){
                         npcs[flerte]->exibirInformacoes(flerte);
                         sleep_seconds(0.1);
                     } else if (tipoFlerte == 2){
-                        player->Presentear(npcs[flerte], 1);
+                        system("cls");
+                        int giveGift = 0;
+                        player->seeItems();
+                        cout << "\nEscolha que presente deseja dar: ";
+                        cin >> giveGift;
+                        player->Presentear(npcs[flerte], giveGift);
                         npcs[flerte]->exibirInformacoes(flerte);
                         sleep_seconds(2);
                     }else{
@@ -263,10 +331,25 @@ int main(){
                 } while (tipoFlerte != 0);
                 break;
             case 4:
-            
+                system("cls");
+                int indexNamoro;
+                for (int i = 0; i < npcs.size(); i++){
+                    npcs[i]->exibirInformacoes(i);
+                }
+                cout << "Quem você deseja pedir em namoro?\n-> ";
+                cin >> indexNamoro;
+                player->pedirNamoro(npcs[indexNamoro]);
                 break;
             case 5:
-            
+                system("cls");
+                int indexCasamento;
+                for (int i = 0; i < npcs.size(); i++){
+                    npcs[i]->exibirInformacoes(i);
+                }
+                cout << "Quem você deseja pedir em casamento?\n-> ";
+                cin >> indexCasamento;
+                player->pedirCasamento(npcs[indexCasamento]);
+                break;
                 break;
             case 6:
                 int s;
@@ -290,6 +373,72 @@ int main(){
                     cin >> u;
                     cout << endl;
                 }while (u != 0);
+                break;
+            case 9:
+                int debug;
+                do{
+                    system("cls");
+                    cout << "\n---------------------- DEBUG MENU ----------------------\n";
+                    cout << "1. Adicionar NPC\n2. Deletar NPC\n3. Adicionar Dinheiro\n0. Sair\n->";
+                    cin >> debug;
+                    switch(debug){
+                        case 1:{
+                            string nomeNPC; 
+                            int idadeNPC, presenteNPC;
+
+                            system("cls");
+                            cout << "\n---------------------- CRIAR NPC ----------------------\n";
+
+                            cout << "Nome do novo pretendente: ";
+                            cin >> nomeNPC;
+
+                            cout << "Idade: ";
+                            cin >> idadeNPC;
+
+                            cout << "\nPresentes: 0.Flores 1.Chocolate 2.Livro 3.Cafe 4.Pizza 5.Videogame 6.Joia 7.Musica 8.Planta 9.Roupa 10.Pelucia 11.Perfume";
+                            cout << "\nQual o seu presente favorito: ";
+                            cin >> presenteNPC;
+
+                            NPC* novoNPC = new NPC(nomeNPC, idadeNPC, presenteNPC);
+                            npcs.push_back(novoNPC);
+
+                            cout << "\nNPC " << nomeNPC << " criado com sucesso!\n";
+                            sleep_seconds(1.5);
+                            break;
+                        }
+                        case 2:
+                            system("cls");
+                            int total = -1;
+                            for (int i = 0; i < npcs.size(); i++){
+                                npcs[i]->exibirInformacoes(i);
+                                total++;
+                            }
+                            int excluir;
+                            cout << "\nDe 0 - " << total << " selecione quem você quer excluir\n-> ";
+                            cin >> excluir;
+
+                            if (excluir >= 0 && excluir < npcs.size()){
+                                cout << "\nNPC " << npcs[excluir]->getName() << " excluido!\n";
+                                delete npcs[excluir]; // libera
+                                npcs.erase(npcs.begin() + excluir); // remove
+                            }else{
+                                cout << "\nInvalido!\n";
+                            }
+
+                            sleep_seconds(1.5);
+                            break;
+                        case 3:
+                            double money;
+                            system("cls");
+                            cout << "\nQuanto de dinheiro deseja adicionar\n-> ";
+                            cin >> money;
+                            player->setMoney(money);
+                            break;
+                        default:
+                            cout << "\nERRO\n";
+                            break;
+                    }
+                }while(debug != 0);
                 break;
             case 0:
                 system("cls");
